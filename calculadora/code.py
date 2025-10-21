ae57725c94976c8fdf31b4a606af8b4804b0a935
Tomas Martinez <tmartinez@utem.cl>
2025-10-21 18:37:17 -0300

    Agregando calculadora a github

---
+++ calculadora/code.py
@@ -0,0 +1,19 @@
+num1 = int(input("Ingresa el primer numero"))
+num2 = int(input("Ingresa el primer numero"))
+
+operacion = input("Ingresa la operacion a realizar")
+
+if operacion == "suma":
+    print("Operacion suma:")
+    print(num1 + num2)
+elif operacion == "resta":
+    print("Operacion resta:")
+    print(num1 - num2)
+elif operacion == "division":
+    print("Operacion division:")
+    print(num1 / num2)
+elif operacion == "multiplicacion":
+    print("Operacion multiplicacion:")
+    print(num1 * num2)
+else: 
+    print("Signo invalido")
