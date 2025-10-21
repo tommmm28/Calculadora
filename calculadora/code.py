6c8a95c8c10a6e308a51b2eead44ec5a17d7947f
Tomas Martinez <tmartinez@utem.cl>
2025-10-21 19:00:47 -0300

    Nuevas funcionalidades

--- calculadora/code.py
+++ calculadora/code.py
@@ -1,23 +1,25 @@
-num1 = int(input("Ingresa el primer numero"))
-num2 = int(input("Ingresa el primer numero"))
-
-operacion = input("Ingresa la operacion a realizar")
-
 while True:
-    if operacion == "suma":
-       print("Operacion suma:")
-       print(num1 + num2)
-    elif operacion == "resta":
-        print("Operacion resta:")
-        print(num1 - num2)
-    elif operacion == "division":
-        print("Operacion division:")
-        print(num1 / num2)
-    elif operacion == "multiplicacion":
-        print("Operacion multiplicacion:")
-        print(num1 * num2)
-    else: 
-        print("Signo invalido")
-        """continuar = input("Desea continuar? si/no") 
-        if continuar == "no":
-            break"""
+     num1 = int(input("Ingresa el primer numero"))
+     num2 = int(input("Ingresa el primer numero"))
+
+     operacion = input("Ingresa la operacion a realizar")
+
+
+     if operacion == "suma":
+         print("Operacion suma:")
+         print(num1 + num2)
+     elif operacion == "resta":
+         print("Operacion resta:")
+         print(num1 - num2)
+     elif operacion == "division":
+         print("Operacion division:")
+         print(num1 / num2)
+     elif operacion == "multiplicacion":
+         print("Operacion multiplicacion:")
+         print(num1 * num2)
+     else: 
+         print("Signo invalido")
+
+     continuar = input("Desea continuar? si/no") 
+     if continuar == "no":
+         break
